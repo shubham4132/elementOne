847f5cd66857fef29dae66bfba8fe60d65f2bd75
shubham4132 <shubhamg3122134@gmail.com>
2026-03-21 22:02:37 +0530

    fix: cookie secure and sameSite for production

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -50,6 +50,8 @@ export const logout = handleAsyncError(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   });
   res.status(200).json({
     success: true,
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,13 +1,15 @@
 export const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  // options for cookies
   const options = {
     expires: new Date(
       Date.now() + process.env.EXPIRE_COOKIE * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   };
+
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     user,
