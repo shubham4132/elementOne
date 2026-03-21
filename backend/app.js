3b36815cb9c847f62ee2f9ee702753d2fe78a698
shubham4132 <shubhamg3122134@gmail.com>
2026-03-21 21:29:36 +0530

    added

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "https://element-10scyrjdf-shubham-goswamis-projects.vercel.app",
+      "https://element-one.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
